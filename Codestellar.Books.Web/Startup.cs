a9b87ba17204419e08064cfa2a78d183aa70394c
Gaurav Madaan <gaurav.madaan@technossus.com>
2016-06-19 14:04:52 +0530

    Initial Commit

---
+++ Codestellar.Books.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Codestellar.Books.Web.Startup))]
+namespace Codestellar.Books.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
